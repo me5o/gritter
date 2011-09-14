dd0840ccc8a5f5c010a2ccd7bd5919f676e33d71
meso <meso@mesolabo.com>
2011-09-14 20:20:46 +0900

    22 89b b 設定追加

--- tweet_daily.rb
+++ tweet_daily.rb
@@ -2,25 +2,28 @@
 $:.unshift(File.dirname(__FILE__))
 require 'gritter'
 
-gritter = Gritter.new($ARGV[0])
+def talk_event(me, event_date, tpl = nil, cal = "default")
+  tpl ||= "[イベント情報]『:title』:date :where #event :desc"
+  dates = event_date.is_a?(Array) ? event_date : [event_date]
+  dates << dates[0] if dates.size == 1
+  sec = 1/24/60/60
+  me.schedule(cal, "start-min" => dates[0], "start-max" => dates[1] + (1 - sec)).each do |event|
+    me.talk event.to_message(tpl), 140
+    sleep 1
+  end
+end
 
-puts "start."
+gritter = Gritter.new($ARGV[0], "./gritter.yml")
+
+puts "start gritter."
 
 #tommorow schedule
 tpl = "明日は『:title』があるぐりっ！ :date :where #gsojp :desc"
-tomorrow = Date.today + 1
-sec = 1/24/60/60
-gritter.schedule("default", "start-min" => tomorrow, "start-max" => tomorrow + (1 - sec)).each do |event|
-  gritter.talk event.to_message(tpl), 140
-  sleep 1
-end
+talk_event(gritter, Date.today + 1, tpl)
 
 #aniversary
 tpl = "今日は:titleぐりっ (-人-) #bigband :desc"
-gritter.schedule("aniversary", "start-min" => Date.today, "start-max" => Date.today + (1 - sec)).each do |event|
-  gritter.talk event.to_message(tpl), 140
-  sleep 1
-end
+talk_event(gritter, Date.today, tpl, "aniversary")
 
 #this week schedule (created after yestarday)
 #nextsunday = Date.today - Date.today.wday + 7
@@ -32,4 +35,17 @@ end
 #collect relative message
 gritter.collect("gsob", Date.today - 1)
 
+puts "start 2289bb."
+
+bb2289 = Gritter.new($ARGV[0], "./2289bb.yml")
+
+#tommorow schedule
+tpl = "明日は『:title』ですっ。 :date :where #2289bb :desc"
+talk_event(bb2289, Date.today + 1, tpl)
+
+#member info
+tpl = "[メンバー情報]『:title』明日です！ :date :where #2289bb :desc"
+talk_event(bb2289, Date.today + 1, tpl, "members")
+
 puts "end."
+
--- tweet_weekly.rb
+++ tweet_weekly.rb
@@ -2,17 +2,37 @@
 $:.unshift(File.dirname(__FILE__))
 require 'gritter'
 
-puts "start."
-
-gritter = Gritter.new($ARGV[0])
-
-#this week schedule
-tpl = "今週 :date は『:title』があるぐりっ！ :where #gsojp :desc".strip 
-nextsunday = Date.today - Date.today.wday + 7
-sec = 1/24/60/60
-gritter.schedule("default", "start-min" => Date.today + 2, "start-max" => nextsunday + (1 - sec)).each do |event|
-  gritter.talk event.to_message(tpl), 140
-  sleep 1
+def talk_event(me, event_date, tpl = nil, cal = "default")
+  tpl ||= "[イベント情報]『:title』:date :where #event :desc"
+  dates = event_date.is_a?(Array) ? event_date : [event_date]
+  dates << dates[0] if dates.size == 1
+  sec = 1/24/60/60
+  me.schedule(cal, "start-min" => dates[0], "start-max" => dates[1] + (1 - sec)).each do |event|
+    me.talk event.to_message(tpl), 140
+    sleep 1
+  end
 end
 
+gritter = Gritter.new($ARGV[0], "./gritter.yml")
+
+puts "start gritter."
+
+#this week schedule
+nextsunday = Date.today - Date.today.wday + 7
+tpl = "今週 :date は『:title』があるぐりっ！ :where #gsojp :desc".strip
+talk_event(gritter, [Date.today + 2, nextsunday], tpl)
+
+puts "start 2289bb."
+
+bb2289 = Gritter.new($ARGV[0], "./2289bb.yml")
+
+#this week schedule
+tpl = "今週は『:title』ですっ。 :date :where #2289bb :desc"
+talk_event(bb2289, [Date.today + 2, nextsunday], tpl)
+
+#member info
+tpl = "[メンバー情報]『:title』是非！ :date :where #2289bb :desc"
+talk_event(bb2289, [Date.today + 2, nextsunday], tpl, "members")
+
 puts "end."
+
